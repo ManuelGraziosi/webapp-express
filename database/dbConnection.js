66a0b9b8324534a76e0ecb87af1d089615cb757b
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-02-04 19:43:07 +0100

    feat: db connection using env completes

---
+++ database/dbConnection.js
@@ -0,0 +1,15 @@
+import mysql2 from "mysql2";
+
+const connection = mysql2.createConnection({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PSWD,
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`Connected to MySql DB`);
+});
+
+export default connection;

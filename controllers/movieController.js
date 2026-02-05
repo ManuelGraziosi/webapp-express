44ebf385e0c5f768fd305de7b9e28ff2a8a4ebe1
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-02-05 15:44:26 +0100

    feat: route Index with real data from db (wip)

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,10 +1,30 @@
-function index(req, res) {
-  return res.json("index");
+import connection from "../database/dbConnection.js";
+
+function index(req, res, next) {
+  const moviesQuery = `
+    SELECT * 
+    FROM movies
+  `;
+
+  connection.query(moviesQuery, (err, results) => {
+    console.log("movieController/index/results", results);
+    if (err) return next(err);
+
+    const replayObject = {
+      info: {
+        length: results.length,
+      },
+      results,
+    };
+    return res.json(replayObject);
+  });
 }
 
 function show(req, res) {
   const id = req.params.id;
-  return res.json(`show: ${id}`);
+  return res.json({
+    message: `show: ${id}`,
+  });
 }
 
 export default { index, show };

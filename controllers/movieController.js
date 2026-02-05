e6eb1c03665e38364cf0da3f2298e4d5802665ed
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-02-05 16:28:12 +0100

    feat: route Show with real data from db (wip)

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,11 +3,11 @@ import { DateTime } from "luxon";
 
 function index(req, res, next) {
   const moviesQuery = `
-    SELECT movies.*, AVG(reviews.vote) AS "avg_vote"
-FROM movies
-INNER JOIN reviews
-WHERE movies.id = reviews.movie_id
-GROUP BY movies.id
+    SELECT movies.*, CAST(AVG(reviews.vote) AS FLOAT) AS "avg_vote"
+    FROM movies
+    LEFT JOIN reviews
+    WHERE movies.id = reviews.movie_id
+    GROUP BY movies.id
   `;
 
   connection.query(moviesQuery, (err, results) => {
@@ -36,8 +36,34 @@ GROUP BY movies.id
 
 function show(req, res) {
   const id = req.params.id;
-  return res.json({
-    message: `show: ${id}`,
+
+  const movieQuery = `
+    SELECT *
+    FROM movies
+    WHERE id = ?
+  `;
+
+  const rewiesQuery = `
+    SELECT *
+    FROM reviews
+    WHERE movie_id = ?
+  `;
+  connection.query(movieQuery, [id], (err, results) => {
+    if (err) return next(err);
+
+    if (results.length === 0) {
+      res.status(404);
+      return res.json({
+        error: "NOT FOUND",
+        messate: "Film non trovato",
+      });
+    }
+
+    const movie = results[0];
+
+    return res.json({
+      movie,
+    });
   });
 }
 

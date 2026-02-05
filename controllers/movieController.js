7c1cb2bdd7cbe00c168d585261694ca72de6a3e7
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-02-05 16:09:25 +0100

    feat: index route with formatted real data completed

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,20 +1,34 @@
 import connection from "../database/dbConnection.js";
+import { DateTime } from "luxon";
 
 function index(req, res, next) {
   const moviesQuery = `
-    SELECT * 
-    FROM movies
+    SELECT movies.*, AVG(reviews.vote) AS "avg_vote"
+FROM movies
+INNER JOIN reviews
+WHERE movies.id = reviews.movie_id
+GROUP BY movies.id
   `;
 
   connection.query(moviesQuery, (err, results) => {
-    console.log("movieController/index/results", results);
     if (err) return next(err);
+
+    const movies = results.map((movie) => {
+      // console.log("movie.created_at", movie.created_at);
+      // console.log("typeof(movie.created_at)", typeof movie.created_at);
+      return {
+        ...movie,
+        image: `${process.env.SRV_PATH}/img/${movie.image}`,
+        created_at: DateTime.fromJSDate(movie.created_at).toISODate(),
+        updated_at: DateTime.fromJSDate(movie.updated_at).toISODate(),
+      };
+    });
 
     const replayObject = {
       info: {
-        length: results.length,
+        length: movies.length,
       },
-      results,
+      movies,
     };
     return res.json(replayObject);
   });
